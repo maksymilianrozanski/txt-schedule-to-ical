676d49b8915e08a3ab6c04e49663ad70271ad4f0
Max <maksymilianrozanski@gmail.com>
2019-09-24 15:12:33 +0200

    add extractDateFromString method

--- src/main/kotlin/TxtLine.kt
+++ src/main/kotlin/TxtLine.kt
@@ -1,3 +1,6 @@
+
+import java.io.IOException
+import java.util.regex.Pattern
 import java.util.stream.Stream
 
 class TxtLine(val textLessonLine: String) {
@@ -20,8 +23,20 @@ class TxtLine(val textLessonLine: String) {
     }
 }
 
-fun stringToStream(string: String): Stream<String> {
+fun lineToStream(string: String): Stream<String> {
     val delimiter = "\t"
     val list = string.split(delimiter)
     return list.stream()
+}
+
+//example input: [Zwęż]	Data Zajęć: 2019-10-05 sobota
+fun extractDateFromString(stringWithDate: String): String {
+    val yearStartIndex = stringWithDate.indexOf("20")
+    val dateAndDay = stringWithDate.subSequence(yearStartIndex, stringWithDate.length - 1)
+    val date = dateAndDay.toString().split(" ")[0]
+    val pattern = Pattern.compile("\\d\\d\\d\\d-\\d\\d-\\d\\d")
+    val matcher = pattern.matcher(date)
+    val matches = matcher.matches()
+    if (!matches) throw IOException("Bad input")
+    return date
 }
--- src/test/kotlin/TxtLineTest.kt
+++ src/test/kotlin/TxtLineTest.kt
@@ -1,8 +1,9 @@
-
 import org.junit.Assert
 import org.junit.Test
+import java.io.IOException
 import java.util.stream.Stream
 import kotlin.streams.toList
+import kotlin.test.assertFailsWith
 
 
 class TxtLineTest {
@@ -11,7 +12,29 @@ class TxtLineTest {
     fun stringToStreamTests() {
         val testString = "fsdffasf\tyrtyrtyrtytryr\tpiopoipiopio"
         val expectedStream = Stream.of("fsdffasf", "yrtyrtyrtytryr", "piopoipiopio")
-        val result = stringToStream(testString)
+        val result = lineToStream(testString)
         Assert.assertEquals(expectedStream.toList(), result.toList())
+    }
+
+    @Test
+    fun extractDateFromStringTest() {
+        val input1 = "Data Zajęć: 2019-10-04 piątek"
+        val input2 = "[Zwęż]\tData Zajęć: 2019-10-06 niedziela"
+
+        val result1 = extractDateFromString(input1)
+        val result2 = extractDateFromString(input2)
+
+        Assert.assertEquals("2019-10-04", result1)
+        Assert.assertEquals("2019-10-06", result2)
+    }
+
+    @Test
+    fun extractDateFromStringBadInputTest() {
+        val badInput = "Data Zajęć: 209-10-04 piątek"
+
+        val exception = assertFailsWith<IOException> {
+            extractDateFromString(badInput)
+        }
+        Assert.assertEquals("Bad input", exception.message)
     }
 }
