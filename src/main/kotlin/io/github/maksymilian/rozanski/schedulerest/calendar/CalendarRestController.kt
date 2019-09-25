e70841938a47911097f1aa25a0b46f6ff5844c0b
Max <maksymilianrozanski@gmail.com>
2019-09-25 14:11:09 +0200

    add ical4j.properties, add getCalendar method to rest controller

--- src/main/kotlin/io/github/maksymilian/rozanski/schedulerest/calendar/CalendarRestController.kt
+++ src/main/kotlin/io/github/maksymilian/rozanski/schedulerest/calendar/CalendarRestController.kt
@@ -5,24 +5,32 @@ import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.Clock
+import java.util.*
+import java.util.stream.Stream
 
 @RestController
 @RequestMapping("/api")
 class CalendarRestController {
 
+    val calendar = GregorianCalendar()
+    val clock = Clock.systemUTC()
+
     @GetMapping("/test")
     fun getHelloWorld(): ResponseEntity<String> {
         val body = "Hello World!!"
         return ResponseEntity.ok().contentLength(body.length.toLong())
-                .contentType(MediaType.parseMediaType("application/octet-stream"))
-                .body(body)
+            .contentType(MediaType.parseMediaType("application/octet-stream"))
+            .body(body)
     }
 
 
     @PostMapping("/cal")
     fun getCalendar(@RequestBody string: String): ResponseEntity<Resource> {
-        val resource = ByteArrayResource(("TestResponse").toByteArray())
+        //TODO: fix bad month values in output (+1 higher)
+        val stream: Stream<String> = string.lines().stream()
+        val resource = ByteArrayResource(generateICalSchedule(stream, calendar, clock).toByteArray())
         return ResponseEntity.ok().contentLength(resource.contentLength())
-                .contentType(MediaType.parseMediaType("application/octet-stream")).body(resource)
+            .contentType(MediaType.parseMediaType("application/octet-stream")).body(resource)
     }
 }
