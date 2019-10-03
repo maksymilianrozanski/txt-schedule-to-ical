10323babe9b418975395aaa30eeb695925d2d192
Max <maksymilianrozanski@gmail.com>
2019-10-03 19:57:36 +0200

    print ical calendar generated from uploaded file to console

--- src/main/resources/static/help.js
+++ src/main/resources/static/help.js
@@ -1,3 +1,26 @@
-function createICal() {
-    console.log("Hello world!!!")
+function init() {
+    document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
+}
+
+function handleFileSelect(event) {
+    const reader = new FileReader();
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0])
+}
+
+function handleFileLoad(event) {
+    let input = event.target.result;
+    generateCalendar(input);
+}
+
+function generateCalendar(inputText) {
+    console.log("Inside generateCalendar");
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:8080/api/cal");
+    request.setRequestHeader("Content-Type", "text/plain");
+    request.send(inputText);
+    request.onload = function () {
+        console.log("Response code: " + request.status);
+        console.log(request.responseText)
+    }
 }
