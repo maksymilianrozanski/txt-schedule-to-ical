d891459e218712e5e8d10b90f6b4e11715272ffc
Max <maksymilianrozanski@gmail.com>
2019-10-03 20:11:46 +0200

    add downloading generated calendar

--- src/main/resources/static/help.js
+++ src/main/resources/static/help.js
@@ -14,13 +14,21 @@ function handleFileLoad(event) {
 }
 
 function generateCalendar(inputText) {
-    console.log("Inside generateCalendar");
     let request = new XMLHttpRequest();
     request.open("POST", "http://localhost:8080/api/cal");
     request.setRequestHeader("Content-Type", "text/plain");
     request.send(inputText);
     request.onload = function () {
-        console.log("Response code: " + request.status);
-        console.log(request.responseText)
+        download("generatedSchedule.ical", request.responseText);
     }
+}
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 }
